164b1646cc9d94d82e63d64ebdadb7153d73ee24
Liman <90157759+LimaniTsa@users.noreply.github.com>
2024-06-20 17:56:48 +0100

    Update control.java

--- control.java
+++ control.java
@@ -3,12 +3,12 @@
 //  Limani Tsatiashvili C22334951
 //  Caitriona McCann C22408674
 //
-//  Topic Meddeler
+//  Topic Modeller
 //
 //  Program Description:
 //  This program asks users to select 2 text files to be compared agaisnt
 //  each other to compare the list of common words to see if the text files
-//  are about simaler topics. Before comparison the files have their stop
+//  are about similar topics. Before comparison the files have their stop
 //  words removed (Words like "a", "the", "where") the user can add stop
 //  words to be removed from the text file
 //  
